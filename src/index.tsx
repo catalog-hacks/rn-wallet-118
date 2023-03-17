54a0c845bdd6e760f7c4f0e1fc27597ab0c3318b
Gowtham Chittemsetty <saigowthamchittemsetty@Gowthams-MacBook-Air.local>
2023-03-17 17:17:31 +0530

    send eth

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {getTokenBalance} from './providers/ethereum/ethereum';
-import {generateWallet} from './wallet/generateWallet';
-import {Wallet} from './wallet/wallet';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { ethProvider, getTokenBalance, sendETH } from './providers/ethereum/ethereum';
+import { generateWallet } from './wallet/generateWallet';
+import { Wallet } from './wallet/wallet';
+import { ethers } from 'ethers';
 
 const First = () => {
   const [wallet, setWallet] = useState<Wallet | null>(null);
   const [phrase, setPhrase] = useState('');
+  const [ethBalance, setETHBalance] = useState('');
 
   return (
     <View>
-      <Text>hello</Text>
       <Button
         title="press me"
         onPress={async () => {
           try {
             console.log('pressed');
-            const {wallet, phrase} = await generateWallet();
+            const { wallet, phrase } = await generateWallet();
 
             setWallet(wallet);
             setPhrase(phrase);
 
-            console.log(
+            console.log('ETH Balance ', await ethProvider.getBalance(wallet.evmAddress()));
+
+            console.log("USDC Balance",
               await getTokenBalance(
                 '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
                 wallet.evmAddress(),
@@ -34,7 +37,6 @@ const First = () => {
           }
         }}
       />
-
       {wallet && (
         <>
           <Text>{wallet.evmAddress()}</Text>
@@ -42,6 +44,18 @@ const First = () => {
           <Text>{phrase}</Text>
         </>
       )}
+      <Button
+        title="Send ETH"
+        onPress={async () => {
+          try {
+            console.log('pressed send');
+            await sendETH({ receiver: '0xB5eDd86d1632D2a59d3ccfF0E49316c16FC13b4B', senderPk: '57bbd81008123e3d998bd32c751b183be4735957c6c27ff19aacd633f9847c98',amount: "0.002" })
+          } catch (error) {
+            console.log('error found at root');
+            console.error(error);
+          }
+        }}
+      />
     </View>
   );
 };
--- src/providers/ethereum/ethereum.ts
+++ src/providers/ethereum/ethereum.ts
@@ -1,6 +1,14 @@
-import {JsonRpcProvider} from '@ethersproject/providers';
-import {ethers} from 'ethers';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Wallet, ethers } from 'ethers';
 import ERC20ABI from '../../constants/erc20abi';
+import { send } from 'process';
+import { address } from 'bitcoinjs-lib';
+
+type sendTxProps = {
+  senderPk: string;
+  receiver: string;
+  amount: string;
+};
 
 export const ethProvider = new JsonRpcProvider(
   'https://goerli.infura.io/v3/b863ead591d54e77be1db79ef34797a3',
@@ -19,3 +27,16 @@ export const getTokenBalance = async (
   const balance = (await tokenContract.balanceOf(senderAddress)).toString();
   return balance;
 };
+
+export const sendETH = async ({ receiver, senderPk, amount }: sendTxProps) => {
+  const wallet = new ethers.Wallet(senderPk, ethProvider);
+  const tx: ethers.providers.TransactionRequest = {
+    to: receiver,
+    value: ethers.utils.parseEther(amount),
+  };
+  console.log('tx: ', tx);
+  const txResponse = await wallet.sendTransaction(tx);
+  console.log('txResponse: ', txResponse);
+};
+
+// 0x7684b983B4b33E6486833152FB0124f1841bd2Ba
